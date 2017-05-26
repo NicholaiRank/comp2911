cd48164ebc4407e63ee80a177a6e696c3392f48f
Maxillary <maxkellymills@gmail.com>
2017-05-26 19:56:06 +1000

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -241,8 +241,15 @@ public class Game extends Application {
     private Scene showNewGameScene(){
     	// Generate new gameboard, player, keypress, interaction and tileset here
     	Player newPlayer = new Player("PLAYER");
-        GameBoardGen ga = new GameBoardGen(10, 5, 1,newPlayer);
-		g = ga.getBoard();
+        GameBoardGen ga;
+        if (room <= 5) ga = new GameBoardGen(10, 5, 1,newPlayer);
+        else if (room <= 10) ga = new GameBoardGen(10, 8, 2,newPlayer);
+        else if (room <= 15) ga = new GameBoardGen(10, 10, 2,newPlayer);
+        else if (room <= 20) ga = new GameBoardGen(10, 12, 3,newPlayer);
+        else if (room <= 25) ga = new GameBoardGen(15, 10, 5,newPlayer);
+        else if (room <= 30) ga = new GameBoardGen(16, 16, 6,newPlayer);
+        else ga = new GameBoardGen(22, 16, 20,newPlayer);
+	g = ga.getBoard();
 		
 		// Get tileset
     	if (room <= 10) tileset = new TileSet(1);
